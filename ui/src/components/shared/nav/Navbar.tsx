972bddeca5ece071b7798091fe69b9292de666f2
DoniLite <yaomessan13@outlook.com>
2025-08-27 13:35:41 +0000

    feat(ui): enhance the ui and the core logic

--- ui/src/components/shared/nav/Navbar.tsx
+++ ui/src/components/shared/nav/Navbar.tsx
@@ -1,27 +1,36 @@
 // import logo from '../../../../assets/ISSGEO_white.png';
+import { Link } from '@tanstack/react-router';
 import darkLogo from '../../../../assets/ISSGEO_dark.png';
 import { Navigation } from './NavigationLinks';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export default function NavBar() {
+  const isMobile = useIsMobile()
   return (
-    <div className='w-full h-[4rem] fixed text-foreground bg-transparent'>
+    <div className='w-full h-[4rem] fixed text-foreground bg-transparent z-20'>
       <div className='container mx-auto p-2 h-full flex items-center justify-between lg:p-4'>
-        <img src={darkLogo} alt='Logo' className='w-12 h-12' />
-        <Navigation />
-        <div className='flex items-center gap-4'>
-          <button
-            type='button'
-            className='bg-muted cursor-pointer text-muted-foreground font-medium px-4 py-2 rounded-md hover:bg-muted/90'
-          >
-            Make Reservation
-          </button>
-          <button
-            type='button'
-            className='bg-secondary cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-secondary/90'
-          >
-            Get started
-          </button>
-        </div>
+        <Link to='.'>
+          <img src={darkLogo} alt='Logo' className='w-12 h-12' />
+        </Link>
+        {!isMobile ? (
+          <>
+            <Navigation />
+            <div className='flex items-center gap-4'>
+              <button
+                type='button'
+                className='bg-muted cursor-pointer text-muted-foreground font-medium px-4 py-2 rounded-md hover:bg-muted/90'
+              >
+                Make Reservation
+              </button>
+              <button
+                type='button'
+                className='bg-secondary cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-secondary/90'
+              >
+                Get started
+              </button>
+            </div>
+          </>
+        ) : ''}
       </div>
     </div>
   );
--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -2,14 +2,58 @@ import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
   component: () => (
-    <div className='relative w-full mx-auto h-screen -z-10'>
-      <div className='h-full w-full grid grid-cols-[70%_30%]'>
+    <div className='relative w-full mx-auto h-screen'>
+      <div className='h-full w-full grid grid-cols-[70%_30%] -z-10'>
         <div className='w-full h-full bg-linear-to-tr from-primary/50 via-secondary to-primary'></div>
         <div className='w-full h-full bg-[url(../assets/coverv1.jpg)] bg-cover bg-center bg-no-repeat'></div>
       </div>
-      <h1 className='absolute inset-0 flex items-center justify-center text-white text-4xl font-bold'>
-        Welcome to ISSGEO
-      </h1>
+      <div className='absolute inset-0 flex items-center justify-center p-4'>
+        <div className='flex gap-4 lg:gap-6 flex-col lg:flex-row'>
+          <div className='flex flex-col relative lg:-left-[12rem] lg:gap-4'>
+            <h1 className='text-white text-4xl font-bold max-w-xl'>
+              YOU CAN ALSO GET A CERTIFICATION TODAY
+            </h1>
+            <span className='text-foreground max-w-xl font-bold text-[16px]'>
+              Notre institut propose une gamme de formation professionnel vous
+              permettant d'ontenir des certifications dont vous avez besoin pour
+              debuter votre carriere professionnelle
+            </span>
+            <div className='flex items-center gap-2 lg:gap-4 mt-4'>
+              <button
+                onClick={() => alert('clicked')}
+                type='button'
+                className='bg-muted cursor-pointer text-muted-foreground font-medium px-4 py-2 rounded-md hover:bg-muted/90'
+              >
+                More information
+              </button>
+              <button
+                type='button'
+                className='bg-primary cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-primary/90'
+              >
+                Begin the adventure
+              </button>
+            </div>
+          </div>
+
+          <form
+            action='#'
+            className='w-full lg:w-lg rounded-md p-2 lg:p-4 shadow h-auto lg:h-[35rem] bg-card lg:absolute lg:right-[1rem] xl:right-[6rem] 2xl:right-[16rem] lg:top-1/5 2xl:top-1/4'
+          >
+            <div className='w-full flex flex-col gap-1'>
+              <span className='text-lg font-bold relative left-3'>
+                Your Name
+              </span>
+              <input
+                type='text'
+                id='name'
+                name='name'
+                className='outline-none focus:ring px-3 py-2 rounded-md border'
+                placeholder='Name'
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   ),
 });
