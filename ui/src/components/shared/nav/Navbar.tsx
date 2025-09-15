91498bd6ef5cc8baf3b21fdb6a3166cffd79c8ef
DoniLite <yaomessan13@outlook.com>
2025-09-15 17:02:40 +0000

    feat: add sidebar navigation links

--- ui/src/components/shared/nav/Navbar.tsx
+++ ui/src/components/shared/nav/Navbar.tsx
@@ -10,8 +10,11 @@ import {
 } from '@/components/ui/sheet';
 import { useEffect, useRef, useState } from 'react';
 import { ThemeToggle } from '../ThemeToogle';
+import LanguageSwitcher from '../LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 
 export default function NavBar() {
+  const { t } = useTranslation();
   const [navClass, setNavClass] = useState(
     'w-full h-[4rem] fixed text-foreground bg-transparent z-20 transition-all'
   );
@@ -39,18 +42,19 @@ export default function NavBar() {
         </Link>
         <Navigation />
         <div className='items-center gap-4 hidden lg:flex'>
+          <LanguageSwitcher />
           <ThemeToggle />
           <button
             type='button'
             className='bg-muted cursor-pointer text-muted-foreground font-medium px-4 py-2 rounded-md hover:bg-muted/90'
           >
-            Make Reservation
+            {t('nav.cta.reservation')}
           </button>
           <button
             type='button'
             className='bg-secondary cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-secondary/90'
           >
-            Get started
+            {t('nav.cta.getStarted')}
           </button>
         </div>
         <MobileNav />
@@ -59,20 +63,26 @@ export default function NavBar() {
   );
 }
 
-
 const MobileNav = () => {
   return (
     <Sheet>
       <SheetTrigger className='block lg:hidden' asChild>
         <div className='w-[1.5rem] flex flex-col gap-1 items-center'>
-          <div className='w-full h-[0.2rem] rounded-xs bg-background'></div>
-          <div className='w-full h-[0.2rem] rounded-xs bg-background'></div>
-          <div className='w-full h-[0.2rem] rounded-xs bg-background'></div>
+          <div className='w-full h-[0.2rem] rounded-xs bg-background dark:bg-foreground'></div>
+          <div className='w-full h-[0.2rem] rounded-xs bg-background dark:bg-foreground'></div>
+          <div className='w-full h-[0.2rem] rounded-xs bg-background dark:bg-foreground'></div>
         </div>
       </SheetTrigger>
       <SheetContent>
-        <SheetTitle>ISSGEO Institute</SheetTitle>
+        <SheetTitle>
+          <div className='w-full p-4 flex gap-2 items-center'>
+            <Link to='.'>
+              <img src={darkLogo} alt='Logo' className='w-6 h-6' />
+            </Link>
+            ISSGEO Institute
+          </div>
+        </SheetTitle>
       </SheetContent>
     </Sheet>
   );
-}
+};
--- ui/src/components/shared/nav/NavigationLinks.tsx
+++ ui/src/components/shared/nav/NavigationLinks.tsx
@@ -7,98 +7,61 @@ import {
   NavigationMenuTrigger,
 } from '@/components/ui/navigation-menu';
 import { Link } from '@tanstack/react-router';
+import getPageLinks from './pageLinks';
+import { useTranslation } from 'react-i18next';
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description:
-      'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-];
-
-const menuBaseClass =
-  'bg-transparent text-primary-foreground hover:bg-transparent focus:bg-transparent hover:underline data-[state=open]:bg-transparent data-[state=open]:focus:bg-transparent data-[state=open]:hover:bg-transparent';
+const menuBaseClass = `
+  bg-transparent text-primary-foreground hover:bg-transparent 
+  focus:bg-transparent hover:underline data-[state=open]:bg-transparent 
+  data-[state=open]:focus:bg-transparent data-[state=open]:hover:bg-transparent
+  `;
 
 export function Navigation() {
+  const { t } = useTranslation();
   return (
     <NavigationMenu className='lg:block hidden' viewport={false}>
       <NavigationMenuList>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={menuBaseClass}>
-            <Link to='.'>Home</Link>
+            <Link to='/'>{t('nav.home.id')}</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuTrigger className={menuBaseClass}>
-            Services
+            {t('nav.services.id')}
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className='grid gap-2 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]'>
               <li className='row-span-3'>
                 <NavigationMenuLink asChild>
-                  <a
+                  <Link
                     className='bg-linear-to-br from-primary/50 via-secondary to-primary flex h-full w-full flex-col justify-end rounded-md p-6 no-underline outline-hidden select-none focus:shadow-md'
-                    href='/'
+                    to='/courses'
                   >
-                    <div className='mt-4 mb-2 text-primary text-lg font-medium'>
-                      Courses and Certifications
+                    <div className='mt-4 mb-2 text-primary dark:text-foreground text-lg font-medium'>
+                      {t('nav.services.titleCard.title')}
                     </div>
                     <p className='text-muted text-sm leading-tight'>
-                      Discover our range of courses and certifications to
-                      enhance your skills and advance your career.
+                      {t('nav.services.titleCard.description')}
                     </p>
-                  </a>
+                  </Link>
                 </NavigationMenuLink>
               </li>
-              <ListItem href='/docs' title='Courses'>
-                Reserve now a spot in our courses.
-              </ListItem>
-              <ListItem href='/docs/installation' title='Calendar'>
-                Browse our calendar for upcoming events and workshops.
-              </ListItem>
-              <ListItem href='/docs/primitives/typography' title='Masters'>
-                We offer a variety of masterclasses to deepen your knowledge.
-              </ListItem>
+              {getPageLinks().services.map((link) => (
+                <ListItem key={link.title} title={link.title} href={link.href}>
+                  {link.description}
+                </ListItem>
+              ))}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuTrigger className={menuBaseClass}>
-            About us
+            {t('nav.about.id')}
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className='grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-              {components.map((component) => (
+              {getPageLinks().about.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
@@ -113,11 +76,11 @@ export function Navigation() {
 
         <NavigationMenuItem>
           <NavigationMenuTrigger className={menuBaseClass}>
-            Our team
+            {t('nav.team.id')}
           </NavigationMenuTrigger>
           <NavigationMenuContent>
             <ul className='grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]'>
-              {components.map((component) => (
+              {getPageLinks().team.map((component) => (
                 <ListItem
                   key={component.title}
                   title={component.title}
@@ -131,7 +94,7 @@ export function Navigation() {
         </NavigationMenuItem>
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={menuBaseClass}>
-            <Link to='.'>FAQ</Link>
+            <Link to='/faq'>FAQ</Link>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenuList>
@@ -149,10 +112,10 @@ function ListItem({
     <li {...props}>
       <NavigationMenuLink
         asChild
-        className='focus:bg-muted-foreground/90 hover:bg-muted-foreground/90 data-[active=true]:bg-muted-foreground/90 data-[active=true]:hover:bg-muted-foreground/90 data-[active=true]:focus:bg-muted-foreground/90 rounded-md'
+        className='focus:bg-muted/10 hover:bg-muted-foreground/10 data-[active=true]:bg-muted-foreground/10 data-[active=true]:hover:bg-muted-foreground/10 data-[active=true]:focus:bg-muted-foreground/10 rounded-md'
       >
         <Link to={href}>
-          <div className='text-sm leading-none font-bold text-primary'>
+          <div className='text-sm leading-none font-bold text-primary dark:text-secondary'>
             {title}
           </div>
           <p className='text-foreground line-clamp-2 text-sm transition-all leading-snug'>
