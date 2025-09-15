c63c225ad5339f2f4a9807f27c45ffec7040b0a8
DoniLite <yaomessan13@outlook.com>
2025-09-15 15:05:35 +0000

    refactor: organize the Hero section code and update the calendar code logic

--- ui/src/components/home/Hero.tsx
+++ ui/src/components/home/Hero.tsx
@@ -1,6 +1,7 @@
 import { Select, SelectContent, SelectTrigger } from '@/components/ui/select';
 import { CalendarDate } from '../shared/Calendar';
 import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
 
 export default function Hero() {
   const { t } = useTranslation();
@@ -31,89 +32,93 @@ export default function Hero() {
           </div>
         </div>
 
-        <form
-          action='#'
-          className='w-full lg:w-[30rem] flex flex-col rounded-md p-2 lg:p-4 shadow h-auto lg:h-[32rem] bg-card lg:absolute lg:right-[1rem] xl:right-[6rem] 2xl:right-[16rem] lg:top-1/5 2xl:top-1/4'
-        >
-          <div className='w-full flex flex-col gap-1'>
-            <span className='text-lg font-bold relative left-3'>Your Name</span>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              className='outline-none focus:ring px-3 py-2 rounded-md border'
-              placeholder='Name'
-            />
-          </div>
-
-          <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
-            <span className='text-lg font-bold relative left-3'>
-              Your Email
-            </span>
-            <input
-              type='text'
-              id='email'
-              name='email'
-              className='outline-none focus:ring px-3 py-2 rounded-md border'
-              placeholder='Email'
-            />
-          </div>
-
-          <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
-            <span className='text-lg font-bold relative left-3'>
-              Your Phone
-            </span>
-            <div className='group rounded-md border flex items-center'>
-              <Select>
-                <SelectTrigger className='w-[40%] h-full px-3 py-2'>
-                  <span className='line-clamp-1'>+226 Burkina Faso</span>
-                </SelectTrigger>
-              </Select>
-              <input
-                type='text'
-                id='phone'
-                name='phone'
-                className='outline-none w-[60%] h-full group-focus:ring px-3 py-2 '
-                placeholder='Phone Number'
-              />
-            </div>
-          </div>
-
-          <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
-            <span className='text-lg font-bold relative left-3'>
-              Pick a Formation
-            </span>
-            <Select>
-              <SelectTrigger className='w-full px-3 py-2'>
-                <span className='line-clamp-1'>Select a Formation</span>
-              </SelectTrigger>
-            </Select>
-          </div>
-
-          <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
-            <span className='text-lg font-bold relative left-3'>
-              Choose a Date
-            </span>
-            <Select>
-              <SelectTrigger className='w-full px-3 py-2'>
-                <span className='line-clamp-1'>
-                  What's your disposability ?
-                </span>
-              </SelectTrigger>
-              <SelectContent>
-                <CalendarDate />
-              </SelectContent>
-            </Select>
-          </div>
-
-          <button
-            type='button'
-            className='bg-primary w-full mt-4 lg:mt-auto cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-primary/90'
-          >
-            Reserve a Place Now
-          </button>
-        </form>
+        <HeroForm />
       </div>
     </div>
+  );
+}
+
+export function HeroForm() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+  return (
+    <form
+      action='#'
+      className='w-full lg:w-[30rem] flex flex-col rounded-md p-2 lg:p-4 shadow h-auto lg:h-[32rem] bg-card lg:absolute lg:right-[1rem] xl:right-[6rem] 2xl:right-[16rem] lg:top-1/5 2xl:top-1/4'
+    >
+      <div className='w-full flex flex-col gap-1'>
+        <span className='text-lg font-bold relative left-3'>Your Name</span>
+        <input
+          type='text'
+          id='name'
+          name='name'
+          className='outline-none focus:ring px-3 py-2 rounded-md border'
+          placeholder='Name'
+        />
+      </div>
+
+      <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
+        <span className='text-lg font-bold relative left-3'>Your Email</span>
+        <input
+          type='text'
+          id='email'
+          name='email'
+          className='outline-none focus:ring px-3 py-2 rounded-md border'
+          placeholder='Email'
+        />
+      </div>
+
+      <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
+        <span className='text-lg font-bold relative left-3'>Your Phone</span>
+        <div className='group rounded-md border flex items-center'>
+          <Select>
+            <SelectTrigger className='w-[40%] h-full px-3 py-2'>
+              <span className='line-clamp-1'>+226 Burkina Faso</span>
+            </SelectTrigger>
+          </Select>
+          <input
+            type='text'
+            id='phone'
+            name='phone'
+            className='outline-none w-[60%] h-full group-focus:ring px-3 py-2 '
+            placeholder='Phone Number'
+          />
+        </div>
+      </div>
+
+      <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
+        <span className='text-lg font-bold relative left-3'>
+          Pick a Formation
+        </span>
+        <Select>
+          <SelectTrigger className='w-full px-3 py-2'>
+            <span className='line-clamp-1'>Select a Formation</span>
+          </SelectTrigger>
+        </Select>
+      </div>
+
+      <div className='w-full flex flex-col gap-1 mt-2 lg:mt-4'>
+        <span className='text-lg font-bold relative left-3'>Choose a Date</span>
+        <Select>
+          <SelectTrigger className='w-full px-3 py-2'>
+            <span className='line-clamp-1'>What's your disposability ?</span>
+          </SelectTrigger>
+          <SelectContent>
+            <CalendarDate
+              date={currentDate}
+              setDate={(date) => {
+                setCurrentDate(date);
+              }}
+            />
+          </SelectContent>
+        </Select>
+      </div>
+
+      <button
+        type='button'
+        className='bg-primary w-full mt-4 lg:mt-auto cursor-pointer text-primary-foreground font-medium px-4 py-2 rounded-md hover:bg-primary/90'
+      >
+        Reserve a Place Now
+      </button>
+    </form>
   );
 }
--- ui/src/components/shared/Calendar.tsx
+++ ui/src/components/shared/Calendar.tsx
@@ -1,14 +1,21 @@
 import { Calendar } from '@/components/ui/calendar';
-import { useState } from 'react';
 
-export function CalendarDate() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+interface CalendarOptions {
+  date: Date;
+  setDate: (date: Date) => void;
+}
 
+export function CalendarDate({ date, setDate }: CalendarOptions) {
   return (
     <Calendar
       mode='single'
       selected={date}
-      onSelect={setDate}
+      onSelect={(dat) => {
+        console.log('new date ====>', dat);
+        if (dat) {
+          setDate(dat);
+        }
+      }}
       className='rounded-md border shadow-sm w-full'
       captionLayout='dropdown-months'
     />
