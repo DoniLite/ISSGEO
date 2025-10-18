e6f086a1702ee7e906728a4343dee164c493250e
DoniLite <yaomessan13@outlook.com>
2025-10-18 05:17:30 +0000

    fix: retrieve the compression system in server

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import { Hono } from 'hono';
 import { render } from './entry-server';
 import { serveStatic } from 'hono/bun';
-import { compress } from 'hono/compress';
+// import { compress } from 'hono/compress';
 import { logger } from 'hono/logger';
 import { HTTPException } from 'hono/http-exception';
 import api from './api/index.controller';
@@ -29,9 +29,9 @@ app.notFound((c) => {
 app.use(logger());
 app.use('/admin/*', authServerMiddleware);
 app.use('/static/*', serveStatic({ root: './' }));
-if (process.env.NODE_ENV === 'production') {
-  app.use(compress());
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(compress());
+// }
 
 app.route('/api', api);
 
